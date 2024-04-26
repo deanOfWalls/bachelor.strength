f275a424232b9b67d07136960d2144ad7e56e6d8
deanOfWalls <deanofwalls@gmail.com>
2024-04-25 23:28:31 -0400

    Setting up test for BMI

--- src/test/java/com/deanofwalls/bachelor_strength/TestBMI.java
+++ src/test/java/com/deanofwalls/bachelor_strength/TestBMI.java
@@ -1,5 +1,7 @@
 package com.deanofwalls.bachelor_strength;
 
+import com.deanofwalls.bachelor_strength.model.UserModel;
+import com.deanofwalls.bachelor_strength.service.FitnessService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,13 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
  * Rename to reflect the specific tests it contains, e.g., PersonServiceTests for tests related to the PersonService.
  */
 @SpringBootTest
-public class SomeTest {
+public class TestBMI {
 
 	@Test
 	void contextLoads() {
 		// Write your tests here using the following format:
-
 		// Given
+
 		// When
 		// Then
 	}
