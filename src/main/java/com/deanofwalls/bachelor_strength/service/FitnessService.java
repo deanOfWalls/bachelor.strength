31cefe59c42eca8f719f86bc5cd51b2e6f1cabf6
deanOfWalls <deanofwalls@gmail.com>
2024-04-25 22:50:36 -0400

    Fixed bmi and protein methods

--- src/main/java/com/deanofwalls/bachelor_strength/service/FitnessService.java
+++ src/main/java/com/deanofwalls/bachelor_strength/service/FitnessService.java
@@ -12,21 +12,18 @@ import org.springframework.stereotype.Service;
 public class FitnessService {
 
     public CalculationsModel calculationsModel(UserModel userModel) {
-        double bmi = calculateBMI();
-        //((10 * (weightInPounds / POUNDS_TO_KILOGRAMS)) + (6.25 * heightInCm) - (5 * age) + 5) * ACTIVITY_FACTOR;
-        double tdee = calculateTDEE(userModel.);
-        int recommendedProtein = calculateProteinIntake(userModel.getWeight());
+        double bmi = calculateBMI(userModel);
+        double tdee = calculateTDEE(userModel);
+        int recommendedProtein = calculateProteinIntake(userModel);
         int recommendedCalories = calculateCaloriesForHypertrophy(userModel); // Assuming you will implement this
 
         return new CalculationsModel(bmi, tdee, recommendedProtein, recommendedCalories);
     }
 
     private double calculateBMI(UserModel userModel) {
-        // Convert height from cm to meters and weight from lbs to kg for BMI calculation
-        double heightInMeters = userModel.getHeight() / 100;
-        double weight= userModel.getWeight();
-        return (weight * 0.453592) / (heightInMeters * heightInMeters);
+        return (userModel.getWeight() * 0.453592) / (Math.pow(userModel.getHeight() / 100, 2));
     }
+
 
     private double calculateTDEE(UserModel userModel) {
         // Placeholder for TDEE calculation
@@ -35,7 +32,7 @@ public class FitnessService {
 
     private int calculateProteinIntake(UserModel userModel) {
         // Example: 1.8 grams per kg of body weight
-        return (int) (weight * 0.453592 * 1.8); // Convert lbs to kg
+        return (int) (userModel.getWeight() * 0.453592 * 1.8); // Convert lbs to kg
     }
 
     private int calculateCaloriesForHypertrophy(UserModel userModel) {
