f4e450325230d7e593c2b428be92740ec0356941
deanOfWalls <deanofwalls@gmail.com>
2024-04-26 22:08:05 -0400

    Working on controller

--- src/main/java/com/deanofwalls/bachelor_strength/controller/FitnessController.java
+++ src/main/java/com/deanofwalls/bachelor_strength/controller/FitnessController.java
@@ -1,19 +1,26 @@
 package com.deanofwalls.bachelor_strength.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.deanofwalls.bachelor_strength.model.CalculationsModel;
+import com.deanofwalls.bachelor_strength.model.UserModel;
+import com.deanofwalls.bachelor_strength.service.FitnessService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST Controller to handle HTTP requests.
  * Rename this class to reflect its purpose, e.g., PersonController if it manages Person entities.
  */
 @RestController
+@RequestMapping("/")
 public class FitnessController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    @Autowired
+    private FitnessService fitnessService;
 
-    // Define other endpoints and request methods here
+    @PostMapping("/calculate")
+    public ResponseEntity<CalculationsModel>
+
+
 }
